892da480151de057fc1efd886eb1a0aacee44aa0
charryzhao5 <charryzhao5@gmail.com>
2025-03-18 05:49:17 +0000

    docs(README): 更新项目描述
    - 修改了 README.md 中的项目描述，添加了对 Nature Flow 项目的简要说明
    - 优化了描述内容，使其更加具体和吸引人

--- nature-flow/.next/static/chunks/webpack.js
+++ nature-flow/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73b4c1e137c8929c"; }
+/******/ 		__webpack_require__.h = function() { return "3bd29237e1f06dfc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
